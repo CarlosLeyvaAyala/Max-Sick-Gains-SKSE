72ab83281403adca3071b8bac3a463f6f8cc57c2
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2023-04-03 09:40:22 -0600

    Detect load/new game

--- plugin.cpp
+++ plugin.cpp
@@ -1,12 +1,24 @@
 #include "logger.h"
 using namespace SKSE;
 using namespace RE;
+using namespace std;
 
-SKSEPluginLoad(const LoadInterface *skse) {
+void OnMessage(MessagingInterface::Message* msg) {
+    auto mt{msg->type};
+
+    if (mt == MessagingInterface::kNewGame)
+        log::info("New game. Configuring Max Sick Gains for first use.");
+    else if (mt == MessagingInterface::kPostLoadGame)
+        log::info("Game was loaded. Applying appearance to nearby NPCs");
+}
+
+SKSEPluginLoad(const LoadInterface* skse) {
     Init(skse);
 
     SetupLog();
-    logger::debug("Max Sick Gains has been installed");
+    log::debug("Max Sick Gains is ready to work");
+
+    GetMessagingInterface()->RegisterListener(OnMessage);
 
     return true;
 }
